033f96ef2e23a61985de5f1d51b029b9bfe1450d
c-eng <ceng@cca.edu>
2018-11-09 22:49:44 +0000

    Adding BaseClass implementation

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""BaseModel module
+"""
+import uuid
+import datetime
+
+
+class BaseModel():
+    """BaseModel class
+    """
+
+    def __init__(self):
+        """BaseModel Initilization
+        """
+        self.id = str(uuid.uuid4())
+        x = datetime.datetime.now()
+        self.created_at = x
+        self.updated_at = x
+
+    def __str__(self):
+        """BaseModel Stringification
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                      self.__dict__)
+
+    def save(self):
+        """BaseModel update
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """BaseModel to_dict function
+        """
+        out = {attr.rsplit('__', 1)[-1]: value for attr, value in
+                self.__dict__.items()}
+        out['__class__'] = self.__class__.__name__
+        out['created_at'] = self.created_at.isoformat()
+        out['updated_at'] = self.updated_at.isoformat()
+        return out
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 """
 
 import unittest
+import datetime
 from models.base_model import BaseModel
 
 
@@ -10,35 +11,36 @@ class testBase(unittest.TestCase):
     """ tests Base class
     """
 
-    def setUp(self):
-        """ setup base test """
-        x = BaseModel()
-
     def test_uuid(self):
         """ uuid test """
+        x = BaseModel()
         self.assertEqual(type(x.id), str)
         y = BaseModel()
         self.assertNotEqual(x.id, y.id)
 
     def test_created_at(self):
         """ test datetime creation """
+        x = BaseModel()
         self.assertIsInstance(x.created_at, datetime.datetime)
         self.assertIsInstance(x.updated_at, datetime.datetime)
         self.assertEqual(x.created_at, x.updated_at)
 
     def test_updated_at(self):
         """ test datetime update """
+        x = BaseModel()
         x.save()
         self.assertNotEqual(x.created_at, x.updated_at)
 
     def test_to_dict(self):
         """ test dictionary rep """
+        x = BaseModel()
         good_dict = {"id": x.id, "created_at": x.created_at.isoformat(),
                      "updated_at": x.updated_at.isoformat(),
                      "__class__": "BaseModel"}
-        self.assertEqual(x.to_dict, good_dict)
+        self.assertEqual(x.to_dict(), good_dict)
 
     def test_str(self):
         """ test str rep """
-        good_str = "[BaseModel] ({}) {}".format(x.id, x.to_dict)
+        x = BaseModel()
+        good_str = "[BaseModel] ({}) {}".format(x.id, x.__dict__)
         self.assertEqual(str(x), good_str)
