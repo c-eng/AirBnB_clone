5437be0728e2e349b8500a5e7522b8cbd1f846e5
talktobrent <talktobrent@gmail.com>
2018-11-09 13:18:33 -0800

    Created unittest

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" command module unittest
+"""
+
+import unittest
+
+
+class testBase(unittest.TestCase):
+    """ tests Base class
+    """
+
+    def setUp(self):
+        """ setup base test """
+        x = BaseModel()
+
+    def test_uuid(self):
+        """ uuid test """
+        self.assertEqual(type(x.id), str)
+        y = BaseModel()
+        self.assertNotEqual(x.id, y.id)
+
+    def test_created_at(self):
+        """ test datetime creation """
+        self.assertIsInstance(x.created_at, datetime.datetime)
+        self.assertIsInstance(x.updated_at, datetime.datetime)
+        self.assertEqual(x.created_at, x.updated_at)
+
+    def test_updated_at(self):
+        """ test datetime update """
+        x.save()
+        self.assertNotEqual(x.created_at, x.updated_at)
+
+    def test_to_dict(self):
+        """ test dictionary rep """
+        good_dict = {"id": x.id, "created_at": x.created_at.isoformat(),
+                "updated_at": x.updated_at.isoformat(),
+                "__class__": "BaseModel"}
+        self.assertEqual(x.to_dict, good_dict)
+
+    def test_str(self):
+        """ test str rep """
+        good_str = "[BaseModel] ({}) {}".format(x.id, x.to_dict)
+        self.assertEqual(str(x), good_str)
